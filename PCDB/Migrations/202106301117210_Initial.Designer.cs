6e58372234f80dae255aac3530763fd7a9aac7f5
Shaun Falconer <shaunf1996@hotmail.co.uk>
2021-06-30 13:09:13 +0100

    Consolidate migrations, added user seed data

--- PCDB/Migrations/202106301117210_Initial.Designer.cs
+++ PCDB/Migrations/202106301117210_Initial.Designer.cs
@@ -6,14 +6,14 @@ namespace PCDB.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class InitialMigration : IMigrationMetadata
+    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202106210209542_InitialMigration"; }
+            get { return "202106301117210_Initial"; }
         }
         
         string IMigrationMetadata.Source
--- PCDB/Migrations/Configuration.cs
+++ PCDB/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace PCDB.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using PCDB.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -17,7 +20,33 @@ namespace PCDB.Migrations
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            if (!context.Roles.Any(r => r.Name.Equals("Admin", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Admin" };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName.Equals("Admin", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "Admin" };
+
+                manager.Create(user, "password");
+                manager.AddToRole(user.Id, "Admin");
+            }
         }
     }
 }
